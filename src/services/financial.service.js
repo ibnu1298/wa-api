a3d3226ffc223470709564ac2ca6d3061f803a46
faizunnazif <faizoen75@gmail.com>
2026-04-20 21:17:28 +0700

    auto create newsheet

--- src/services/financial.service.js
+++ src/services/financial.service.js
@@ -6,7 +6,54 @@ const auth = new google.auth.GoogleAuth({
 });
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+function getSheetName() {
+  const now = new Date();
 
+  return new Intl.DateTimeFormat("id-ID", {
+    timeZone: "Asia/Jakarta",
+    month: "short",
+    year: "numeric",
+  })
+    .format(now)
+    .replace(" ", "-"); // contoh: Apr-2026
+}
+async function createSheetIfNotExists(sheets, sheetName) {
+  const existingSheets = await getSheetList(sheets);
+
+  if (existingSheets.includes(sheetName)) return;
+
+  await sheets.spreadsheets.batchUpdate({
+    spreadsheetId: SPREADSHEET_ID,
+    requestBody: {
+      requests: [
+        {
+          addSheet: {
+            properties: {
+              title: sheetName,
+            },
+          },
+        },
+      ],
+    },
+  });
+
+  // 👉 tambah header
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: `${sheetName}!A1`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [["Tanggal", "Kategori", "Item", "Nominal", "Tipe", "Sumber"]],
+    },
+  });
+}
+async function getSheetList(sheets) {
+  const res = await sheets.spreadsheets.get({
+    spreadsheetId: SPREADSHEET_ID,
+  });
+
+  return res.data.sheets.map((s) => s.properties.title);
+}
 async function saveToSheet(data, senderNumber) {
   const client = await auth.getClient();
 
@@ -19,9 +66,14 @@ async function saveToSheet(data, senderNumber) {
     timeZone: "Asia/Jakarta",
   });
 
+  const sheetName = getSheetName();
+
+  // 🔥 pastikan sheet ada
+  await createSheetIfNotExists(sheets, sheetName);
+
   await sheets.spreadsheets.values.append({
     spreadsheetId: SPREADSHEET_ID,
-    range: "ReportSheet!A:F",
+    range: `${sheetName}!A:F`,
     valueInputOption: "USER_ENTERED",
     requestBody: {
       values: [
