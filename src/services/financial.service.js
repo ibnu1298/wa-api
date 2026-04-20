7e0a97df98f5ca7b13ce452b78381a0e15b85556
faizunnazif <faizoen75@gmail.com>
2026-04-20 21:45:25 +0700

    add resume

--- src/services/financial.service.js
+++ src/services/financial.service.js
@@ -46,7 +46,34 @@ async function createSheetIfNotExists(sheets, sheetName) {
       values: [["Tanggal", "Kategori", "Item", "Nominal", "Tipe", "Sumber"]],
     },
   });
+  const categories = process.env.CATEGORIES.split(",").map((c) => c.trim());
+
+  // isi kolom H & I
+  const categoryRows = categories.map((cat, index) => {
+    const rowIndex = index + 2; // mulai dari baris 2
+
+    return [cat, `=SUMIFS(D$2:D,B$2:B,H${rowIndex},E$2:E,"pengeluaran")`];
+  });
+
+  await sheets.spreadsheets.values.update({
+    spreadsheetId: SPREADSHEET_ID,
+    range: `${sheetName}!H1:I1`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [["Kategori", "Pengeluaran"]],
+    },
+  });
+
+  await sheets.spreadsheets.values.update({
+    spreadsheetId: SPREADSHEET_ID,
+    range: `${sheetName}!H2:I${categories.length + 1}`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: categoryRows,
+    },
+  });
 }
+
 async function getSheetList(sheets) {
   const res = await sheets.spreadsheets.get({
     spreadsheetId: SPREADSHEET_ID,
